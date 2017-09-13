9cce1daa4f034e7e568a28c7d30d0af6bc3aab9a
krishnagajavelli <31927445+krishnagajavelli@users.noreply.github.com>
2017-09-13 21:08:16 +0530

    y

---
+++ src/StudentGroup.java
@@ -0,0 +1,175 @@
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * A fix-sized array of students
+ * array length should always be equal to the number of stored elements
+ * after the element was removed the size of the array should be equal to the number of stored elements
+ * after the element was added the size of the array should be equal to the number of stored elements
+ * null elements are not allowed to be stored in the array
+ * 
+ * You may add new methods, fields to this class, but DO NOT RENAME any given class, interface or method
+ * DO NOT PUT any classes into packages
+ *
+ */
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+	ArrayList<Student> ar = new ArrayList<Student>(Arrays.asList(students));
+	
+	/**
+	 * DO NOT remove or change this constructor, it will be used during task check
+	 * @param length
+	 */
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+	@Override
+	public Student[] getStudents() {
+		// Add your implementation here
+		System.out.println(ar);
+		return null;
+	}
+
+	@Override
+	public void setStudents(Student[] students) {
+		
+		// Add your implementation here
+		if(ar.contains(null))
+				{
+				throw new IllegalArgumentException();
+				}
+	}
+
+	@Override
+	public Student getStudent(int index) {
+		// Add your implementation here
+		if(ar.contains(index))
+		{
+		System.out.println(ar.get(index));
+		}
+		return null;
+	}
+
+	@Override
+	public void setStudent(Student student, int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void addFirst(Student student) {
+		// Add your implementation here
+		ar.add(0,student);
+	}
+
+	@Override
+	public void addLast(Student student) {
+		// Add your implementation here
+		int n=ar.size();
+		ar.add(n-1,student);
+	}
+
+	@Override
+	public void add(Student student, int index) {
+		// Add your implementation here
+		ar.add(index,student);
+	}
+
+	@Override
+	public void remove(int index) {
+		// Add your implementation here
+		ar.remove(index);
+	}
+
+	@Override
+	public void remove(Student student) {
+		// Add your implementation here
+		ar.remove(student);
+	}
+
+	@Override
+	public void removeFromIndex(int index) {
+		// Add your implementation here
+		for(int i=index;i<ar.size();i++)
+	{
+	ar.remove(i);
+	}
+	}
+
+	@Override
+	public void removeFromElement(Student student) {
+		// Add your implementation here
+		
+	for(int i=0;i<ar.size();i++)
+	{
+	ar.remove(student);
+	}
+	}
+
+	@Override
+	public void removeToIndex(int index) {
+		// Add your implementation here
+		for(int i=0;i<ar.size();i++)
+	{
+	ar.remove(index);
+	}
+	}
+
+	@Override
+	public void removeToElement(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void bubbleSort() {
+		// Add your implementation here
+	}
+
+	@Override
+	public Student[] getByBirthDate(Date date) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getNearBirthDate(Date date, int days) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public int getCurrentAgeByDate(int indexOfStudent) {
+		// Add your implementation here
+		return 0;
+	}
+
+	@Override
+	public Student[] getStudentsByAge(int age) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getStudentsWithMaxAvgMark() {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student getNextStudent(Student student) {
+		// Add your implementation here
+		return null;
+	}
+}
